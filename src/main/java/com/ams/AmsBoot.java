997b0072c78f18fd341c7b0a1f3f4d19dc837d68
samirbist <samir.bist@gmail.com>
2018-01-06 21:20:22 +0530

    added spring boot

---
+++ src/main/java/com/ams/AmsBoot.java
@@ -0,0 +1,13 @@
+package com.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmsBoot.class, args);
+    }
+
+}
